e0b79b132ba6201e15b68eaadcf36a3a7694fee7
Brice Saint-Michel <bsaintmichel@gmail.com>
2024-06-05 11:55:05 +0200

    Cumulative plots + Tweaks

--- dqn/display.py
+++ dqn/display.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 import typing
 import sys
+from params import *
 
 
 class Plotter():
@@ -10,8 +12,9 @@ class Plotter():
     fig_data_list = []
 
     @classmethod
-    def plot_data_gradually(cls, data_name: str, data: list[float,int],
-                            show_result: bool=False) -> None:
+    def plot_data_gradually(cls, data_name: str, data: list[float,int] | np.ndarray,
+                            show_result: bool=False, cumulative=False,
+                            rolling: int=0) -> None:
         """
         Plots gradually the data as a function of time. Each data is plotted in
         its own figure
@@ -22,10 +25,18 @@ class Plotter():
             data (list[float,int]): the data you want to plot
             show_result (bool, optional): true if the data won't change anymore.
                                           Defaults to False.
+            cumulative (bool, default=False) : plot the _cumulative sum_ of
+                                            a variable instead of the variable instead.
+            rolling (int, default=0) : plots a _rolling average_ of a variable
+                                            instead of a variable
         """
 
         # We get the correct Figure object
         title = 'Result' if show_result else 'Training ...'
+        if cumulative: data = np.cumsum(data)
+        if rolling:
+            dummy = np.convolve(np.ones_like(data), np.ones(rolling)/rolling, mode='valid')
+            data = np.convolve(data, np.ones(rolling)/rolling, mode='valid')/dummy
 
         if data_name not in cls.fig_data_list:
             cls.fig_data_list.append(data_name)
@@ -38,45 +49,85 @@ class Plotter():
         else:
             ax = plt.figure(cls.fig_data_list.index(data_name) + 1).gca()
             ax._children[0].set_data(np.arange(len(data)), data)
-            ax.axis([0, len(data), 0, max(data)])
+            ax.axis([0, len(data)+1, 0, max(data)+0.001])
 
         plt.pause(0.001)  # pause a bit so that plots are updated
 
         if show_result:
             plt.show()
 
+        return ax
 
-def pretty_print(arrays : tuple, transpose=False, names=None):
-    ''' Brice wants pretty prints of arrays
-    or tuples of arrays. The function is a bit ugly, but it works.
-    '''
 
-    n_rows = 0
-    if names is None:
-        names = [f'Table {no}' for no in range(len(arrays))]
+def plot_success_rate(episode_rewards:list, rolling=10):
 
-    for array, name in zip(arrays, names):
-        array = np.atleast_2d(array)
-        if transpose: array = array.T
+    roll_rewards = np.convolve(np.array(episode_rewards),
+                               np.ones(rolling)/rolling, mode='same')
+    ax = Plotter().plot_data_gradually('RollingRewards', data=roll_rewards)
 
-        n_char_table = 7 * array.shape[1]
-        n_char_name = len(name)
-        left_pad = '-'*((n_char_table - n_char_name)//2)
-        right_pad = '-'*((1 + n_char_table - n_char_name)//2)
 
-        pretty_str = left_pad + name + right_pad + '\n'
+def dqn_diagnostics(agent,
+                    action_batch:torch.Tensor,
+                    best_action:torch.Tensor,
+                    state_batch:torch.Tensor,
+                    reward_batch:torch.Tensor,
+                    all_next_states:torch.Tensor,
+                    state_action_values:torch.Tensor,
+                    future_state_values:torch.Tensor,
+                    best_action_values:torch.Tensor):
+    """
+        DQN_diagnostics (plenty of things) : allows you to view
+        detailed diagnotics on the DQN model in the terminal.
 
-        for row in array:
-            n_rows += 1
-            pretty_str += ''.join([f'{val:.2f} | '  for val in row])
-            pretty_str += '\n'
-        pretty_str += '-' *  n_char_table + '\n'
-        n_rows += 2
+        ARGS
+        ----
+        * agent : your agent
+        * action_batch, best_action, state_batch, all_next_states,
+        state_action_values, future_state_values, best_action_values : things
+        that are usually available from optimize, so call this function
+        from your "optimize" in the agent.
 
-        print(pretty_str, end='')
+    """
 
-    # for row in range(n_rows+1):
-        # sys.stdout.write("\033[F") # Cursor up one line
+    with torch.no_grad():
+        action_dict = {0:'←', 1:'↓', 2:'→', 3:'↑'}
+        action_arrows = [action_dict[elem.item()] for elem in action_batch]
+        best_act_arrs = [action_dict[elem.item()] for elem in best_action]
+
+        states_str     = 'Current state ' + ' | '.join([f"{elem:5.0f}" for elem in state_batch])
+        action_str     = 'Current action' + ' | '.join([f"    {elem}"   for elem in action_arrows])
+        reward_str     = 'Reward (s,a)  ' + ' | '.join([f"{elem:5.0f}" for elem in reward_batch])
+        next_state_str = 'Future state  ' + ' | '.join([f"{elem:5.0f}"  for elem in all_next_states])
+        current_Q_str  = 'Current Q     ' + ' | '.join([f"{elem:+4.2f}" for elem in torch.squeeze(state_action_values)])
+        Q_left_str     = 'Estimated Q ← ' + ' | '.join([f"{elem:+4.2f}" for elem in future_state_values[:,0]])
+        Q_down_str     = 'Estimated Q ↓ ' + ' | '.join([f"{elem:+4.2f}" for elem in future_state_values[:,1]])
+        Q_right_str    = 'Estimated Q → ' + ' | '.join([f"{elem:+4.2f}" for elem in future_state_values[:,2]])
+        Q_up_str       = 'Estimated Q ↑ ' + ' | '.join([f"{elem:+4.2f}" for elem in future_state_values[:,3]])
+        expected_Q_str = 'Best known Q  ' + ' | '.join([f"{elem:+4.2f}" for elem in best_action_values])
+        best_act_str   = 'Bst futr actn ' + ' | '.join([f"    {elem}"   for elem in best_act_arrs])
+
+        loss = agent.losses[-1]
+        step = agent.steps_done
+        rewards = np.sum(agent.rewards)
+        eps = EPS_END + (EPS_START - EPS_END) * np.exp(-agent.steps_done / EPS_DECAY)
+
+        print("\033[A"*16)
+        print(f'\nStep {step:4.0f}, \t loss : {float(loss):5.2e}, \t rewards : {rewards:3.0f}, \t epsilon : {eps:.3f}')
+        print('-'*len(states_str))
+        print(states_str)
+        print(action_str)
+        print(reward_str)
+        print(next_state_str)
+        print(current_Q_str)
+        print(Q_left_str)
+        print(Q_down_str)
+        print(Q_right_str)
+        print(Q_up_str)
+        print(expected_Q_str)
+        print(best_act_str)
+        print('-'*len(states_str))
+
+
 
 
 if __name__ == '__main__':
--- dqn/main.py
+++ dqn/main.py
@@ -7,12 +7,11 @@ env = Environment(gym.make("FrozenLake-v1", render_mode='human', is_slippery=Fal
 env.env.metadata['render_fps'] = RENDER_FPS
 agt = agent.DQNAgentBase(1, env.env.action_space.n)
 
-episode_durations = []
 for _ in range(NUM_EPISODES):
-    agt.steps_done = sum(episode_durations)
-    episode_durations.append(env.run_episode(agt))
+    agt.episode_duration.append(env.run_episode(agt))
+    agt.episode_rewards.append(agt.rewards[-1])
+    agt.episode += 1
 
-
-print(f"Average episode duration: {sum(episode_durations) / len(episode_durations)}")
+print(f"Average episode duration: {sum(agt.episode_duration) / len(agt.episode_duration) }")
 input('Press any key to close')
 env.env.close()
