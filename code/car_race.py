f72fa61167e00840a83cb01893d2a4c1a3d2b35f
Brice Saint-Michel <bsaintmichel@gmail.com>
2024-06-06 17:03:48 +0200

    Print Device during car race

--- code/car_race.py
+++ code/car_race.py
@@ -1,18 +1,19 @@
 import gymnasium as gym
 from environment import Environment
-from params import NUM_EPISODES, RENDER_FPS
+from params import NUM_EPISODES, RENDER_FPS, DEVICE
 import car_agent as agent
 
 
 # Initialize Environment
 env = Environment(gym.make("CarRacing-v2",render_mode='rgb_array', continuous=False))
 env.env.metadata['render_fps'] = RENDER_FPS
+print('\n~~~~~CAR RACING USING {DEVICE} ~~~~~')
 
 # Initialize Agent
 agt = agent.CarDQNAgent(env.env.action_space.n, dropout_rate=0.1)
 # agt.load_model("./models/0605_1015DQNAgentObs")
 
-try :
+try:
     for _ in range(NUM_EPISODES):
         env.run_episode(agt)
         agt.end_episode()
