5ea391bc53b4f90fc391c785c67da8a19f9a9493
Brice Saint-Michel <bsaintmichel@gmail.com>
2024-06-13 14:36:09 +0200

    update obs agent

--- code/frozen_agent.py
+++ code/frozen_agent.py
@@ -277,30 +277,15 @@ class FrozenDQNAgentObs(FrozenDQNAgentBase):
 
         if len(self.memory) < BATCH_SIZE: return 0
 
-        transitions = self.memory.sample(BATCH_SIZE)
-
-        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-        # detailed explanation). This converts batch-array of Transitions
-        # to Transition of batch-arrays.
-
-        batch = Transition(*zip(*transitions)) # Needs to pass this from buffer class
-
-        # Compute a mask of non-final states and concatenate the batch elements
-        # (a final state would've been the one after which simulation ended)
-
-        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                            batch.next_state)), device=DEVICE, dtype=torch.bool)
-        non_final_next_states = torch.cat([s for s in batch.next_state
-                                                    if s is not None])
-
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
+        (state_batch,
+         action_batch,
+         next_state_batch,
+         reward_batch,
+         not_done_batch) = self.memory.sample(BATCH_SIZE)
 
         # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
         # columns of actions taken. These are the actions which would've been taken
         # for each batch state according to policy_net
-
         state_action_values = self.policy_net(state_batch).gather(1, action_batch)
 
         # Compute V(s_{t+1}) for all next states.
@@ -308,23 +293,50 @@ class FrozenDQNAgentObs(FrozenDQNAgentBase):
         # on the "older" target_net; selecting their best reward with max(1).values
         # This is merged based on the mask, such that we'll have either the expected
         # state value or 0 in case the state was final.
-
-        next_state_values = torch.zeros(BATCH_SIZE, device=DEVICE)
+        rewards_tensor = torch.tile(reward_batch, (1,4)).to(DEVICE)
 
         with torch.no_grad():
-            result = self.target_net(non_final_next_states)
-            next_state_values[non_final_mask] = result.max(1).values
+            future_state_values = rewards_tensor + not_done_batch \
+                * self.target_net(next_state_batch) * GAMMA
 
-        # Compute the expected Q values
-        expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+            best_action = future_state_values.argmax(1)
+            best_action_values = future_state_values.max(1).values.unsqueeze(-1)
 
-        # Compute Huber loss
-        criterion = nn.SmoothL1Loss()
-        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
-        # print(f"Loss: {float(loss)}")
-        # print(f'Reward in batch:  {reward_batch.sum()}')
+        # Compute MSE loss
 
+        loss = self.lossfun(state_action_values, best_action_values)
         self.losses.append(float(loss))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
+
+        rwd_ep = self.episode_rewards[-1]
+        lr = self.scheduler.optimizer.param_groups[0]['lr']
+        act = self.last_action.item()
+
+        # dqn_diagnostics(self,
+        #                 action_batch,
+        #                 best_action,
+        #                 state_batch,
+        #                 reward_batch,
+        #                 next_state_batch,
+        #                 state_action_values,
+        #                 future_state_values,
+        #                 best_action_values)
+
+        # Fancy print
+        print(f" 🎄  🎄  || {'t':7s} | {'Step':7s} | {'Episode':15s} | {'Loss':8s} |" \
+            + f" {'ε':7s} | {'η':8s} | {'Rwd/ep':7s} | {'Action'}")
+        print(f" 🎄  🎄  || " \
+            + f'{self.time:7.1f} | {self.steps_done:7.0f} | ' \
+            + f'{self.episode:7.0f} / {NUM_EPISODES:5.0f} | ' \
+            + f'{self.losses[-1]:.2e} | {self.epsilon:7.4f} |'\
+            + f' {lr:.2e} | {rwd_ep:7.2f} | {act:7.0f}')
+        print("\033[F"*2, end='')
 
         #Plotting
         if self.steps_done % DISPLAY_EVERY == 0:
